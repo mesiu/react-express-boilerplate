593f837e25363acf6e7d32feb48434710f4db982
Melissa Siu <melissa.siu@gmail.com>
2017-05-24 23:21:24 +1000

    feat(react-router): Add in React Router example

--- src/client.js
+++ src/client.js
@@ -2,13 +2,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './components/App/App';
 
 const render = (Component) => {
   ReactDOM.render(
     <AppContainer>
-      <Component />
+      <Router>
+        <Component />
+      </Router>
     </AppContainer>,
     document.getElementById('root'),
   );
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,19 @@
 import React from 'react';
+import { Route, Link } from 'react-router-dom';
 
-const App = () => <h1>Hello, world!</h1>;
+const Home = () => <h1>Home page</h1>;
+
+const About = () => <h1>About page</h1>;
+
+const App = () => (
+  <div>
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+    </nav>
+    <Route exact path="/" component={Home} />
+    <Route path="/about" component={About} />
+  </div>
+);
 
 export default App;
